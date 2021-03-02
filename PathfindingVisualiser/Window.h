4c193ec0c28928c580b25e558ad40988c852378f
Sylas Coker <60179137+Syycoker@users.noreply.github.com>
2021-03-02 21:39:43 +0000

    created window.h

---
+++ PathfindingVisualiser/Window.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+#include <SDL/SDL.h>
+
+class Window {
+public:
+	Window(const std::string &title, int width, int height);
+	~Window();
+	bool isRunning(){
+		return running;
+	}
+
+private:
+	std::string title;
+	int width = 800;
+	int height = 600;
+	bool running = true;
+	SDL_Window* window = nullptr;
+	bool init();
+};
