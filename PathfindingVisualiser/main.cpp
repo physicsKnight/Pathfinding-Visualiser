043d8bf8d872cbdcc8e3d1b1e4c06a230b623561
sharifee <34819448+physicsKnight@users.noreply.github.com>
2021-03-02 21:51:38 +0000

    Updated main.cpp

--- PathfindingVisualiser/main.cpp
+++ PathfindingVisualiser/main.cpp
@@ -1,7 +1,21 @@
+#pragma once
+
+#include <string>
 #include <SDL/SDL.h>
 
-int main(int argc, char** argv) {
-	SDL_Init(SDL_INIT_EVERYTHING);
+class Window {
+public:
+    Window(const std::string &title, int width, int height);
+    ~Window();
+    bool isRunning(){
+        return running;
+    }
 
-	return 0;
-}
+private:
+    std::string title;
+    int width = 800;
+    int height = 600;
+    bool running = true;
+    SDL_Window* window = nullptr;
+    bool init();
+};
